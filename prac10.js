5f5bb69ddb0cb2b8bb90bc1cc520c7ec0e1d32f9
Zenia Muqadas <zeniaabdulhyee@gmail.com>
2026-09-21 21:06:36 +0500

    prac10.js

---
+++ prac10.js
@@ -0,0 +1,114 @@
+let arr = [1, 2, 4, 5, 7]
+//  Index  0, 1, 2, 3, 4
+
+arr[0] = 5666;
+// console.log(arr, typeof arr);
+// console.log(arr.length)
+
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+
+ 
+// let numbers = [1, 2, 3, 4, 5] 
+// numbers.splice(1, 2)    
+// numbers.splice(1, 3)  
+// numbers.splice(1, 3, 222, 333) 
+// (4) [1, 222, 333, 5]
+let a = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key]; 
+//         console.log(key, element)
+//     }
+// }
+
+for (const value of a) {
+    console.log(value)
+}
+let arr = [1, 13, 5 ,7, 11];
+// let newArr = []
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newArr.push(element**2)
+// }
+
+let newArr = arr.map((e, index, array)=>{
+    return e**2
+})
+
+console.log(newArr)
+const greaterThanSeven = (e)=>{
+    if(e>7){
+        return true
+    }
+    return false
+}
+console.log(arr.filter(greaterThanSeven))
+
+let arr2 = [1,2,3,4,5,6]
+
+const red = (a, b)=>{
+    return a+b
+}
+
+console.log(arr2.reduce(red))
+console.log("Hello world")
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
+console.log("Harry")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green";
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green";
+}) 
+let a = 7
+
+function factorial(number){
+   let arr = Array.from(Array(number+1).keys())
+   let c = arr.slice(1,).reduce((a, b)=> a*b )
+   return c
+}
+
+
+function facFor(number){
+    let fac = 1;
+    for (let index = 1; index <= number; index++) {
+        fac = fac * index
+    }
+    return fac
+}
+console.log(factorial(a))
+console.log(facFor(a))
